5dfe1f8955292826d1d90190fb3088213a1f346b
Konrad Kreczko <konradkreczko@gmail.com>
2023-09-01 00:55:35 +0200

    feat: Added function that empty grid of pixels

--- src/main.ts
+++ src/main.ts
@@ -8,8 +8,27 @@ const generateGrid = (dimension: number, selector: string) => {
       "style",
       `width: ${squareSize}%; height: ${squareSize}; padding: 0; margin: 0;`
     );
+    gridSquare.setAttribute("class", "pixel");
     gridContainer.appendChild(gridSquare);
   }
 };
 
+const emptyGrid = (selector: string) => {
+  const gridContainer = document.querySelector(selector) as HTMLDivElement;
+
+  while (gridContainer.firstChild) {
+    gridContainer.removeChild(gridContainer.firstChild);
+  }
+};
+
 generateGrid(16, "#grid-container");
+emptyGrid("#grid-container");
+generateGrid(32, "#grid-container");
+
+const pixels = document.querySelectorAll(".pixel");
+
+pixels.forEach((pixel) => {
+  pixel.addEventListener("mouseover", (e: Event) => {
+    (e.target as HTMLDivElement).style.backgroundColor = "gray";
+  });
+});
