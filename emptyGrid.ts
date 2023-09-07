b1376d301f4a28f40d1da75051d8bccbd3565d2d
Konrad Kreczko <konradkreczko@gmail.com>
2023-09-08 01:40:18 +0200

    fix: fix

--- emptyGrid.ts
+++
@@ -1,9 +0,0 @@
-const emptyGrid = (selector: string) => {
-  const gridContainer = document.querySelector(selector) as HTMLDivElement;
-
-  while (gridContainer.firstChild) {
-    gridContainer.removeChild(gridContainer.firstChild);
-  }
-};
-
-export default emptyGrid;
--- generateRandomRGB.ts
+++
@@ -1,8 +0,0 @@
-const generateRandomColor = (): string => {
-  const redValue = Math.floor(Math.random() * 255);
-  const greenValue = Math.floor(Math.random() * 255);
-  const blueValue = Math.floor(Math.random() * 255);
-  return `rgba(${redValue},${greenValue},${blueValue})`;
-};
-
-export default generateRandomColor;
