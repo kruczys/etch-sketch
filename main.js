412e19d116221b6339969cf2e57421c7120d7adf
Konrad Kreczko <konradkreczko@gmail.com>
2023-09-08 01:45:34 +0200

    fix: fix 3

--- main.js
+++ main.js
@@ -9,36 +9,37 @@ var currMode = "random-rgb";
 (0, generateGrid_1.default)(startDimension, "#grid-container");
 (0, changeMode_1.default)(currMode);
 sizeChangeButton.addEventListener("click", function () {
-    var newGridDimension = prompt("What grid dimension are you interested in?");
-    startDimension = newGridDimension ? +newGridDimension : startDimension;
-    (0, generateGrid_1.default)(newGridDimension && +newGridDimension <= 100 ? +newGridDimension : 100, "#grid-container");
-    (0, changeMode_1.default)(currMode);
+  var newGridDimension = prompt("What grid dimension are you interested in?");
+  startDimension = newGridDimension ? +newGridDimension : startDimension;
+  (0,
+  generateGrid_1.default)(newGridDimension && +newGridDimension <= 100 ? +newGridDimension : 100, "#grid-container");
+  (0, changeMode_1.default)(currMode);
 });
 emptyCanvasButton.addEventListener("click", function () {
-    (0, generateGrid_1.default)(startDimension, "#grid-container");
-    (0, changeMode_1.default)(currMode);
+  (0, generateGrid_1.default)(startDimension, "#grid-container");
+  (0, changeMode_1.default)(currMode);
 });
 var rainbowModeInput = document.querySelector("#random-rgb");
 var blackModeInput = document.querySelector("#black-mode");
 var darkeningModeInput = document.querySelector("#darkening-mode");
 rainbowModeInput.addEventListener("click", function () {
-    currMode = "random-rgb";
-    (0, changeMode_1.default)(currMode);
-    rainbowModeInput.checked = true;
-    blackModeInput.checked = false;
-    darkeningModeInput.checked = false;
+  currMode = "random-rgb";
+  (0, changeMode_1.default)(currMode);
+  rainbowModeInput.checked = true;
+  blackModeInput.checked = false;
+  darkeningModeInput.checked = false;
 });
 blackModeInput.addEventListener("click", function () {
-    currMode = "black-mode";
-    (0, changeMode_1.default)(currMode);
-    rainbowModeInput.checked = false;
-    blackModeInput.checked = true;
-    darkeningModeInput.checked = false;
+  currMode = "black-mode";
+  (0, changeMode_1.default)(currMode);
+  rainbowModeInput.checked = false;
+  blackModeInput.checked = true;
+  darkeningModeInput.checked = false;
 });
 darkeningModeInput.addEventListener("click", function () {
-    currMode = "darkening-mode";
-    (0, changeMode_1.default)(currMode);
-    rainbowModeInput.checked = false;
-    blackModeInput.checked = false;
-    darkeningModeInput.checked = true;
+  currMode = "darkening-mode";
+  (0, changeMode_1.default)(currMode);
+  rainbowModeInput.checked = false;
+  blackModeInput.checked = false;
+  darkeningModeInput.checked = true;
 });
